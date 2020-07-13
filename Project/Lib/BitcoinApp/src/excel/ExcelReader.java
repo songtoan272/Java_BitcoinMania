3ecf29c78d150fa1c6b699f9d10570582b682fd7
songtoan272 <toancs272@gmail.com>
2020-07-13 19:42:01 +0200

    Live graph auto update + threshold lines + tooltips + ExcelReader

--- Project/Lib/BitcoinApp/src/excel/ExcelReader.java
+++ Project/Lib/BitcoinApp/src/excel/ExcelReader.java
@@ -13,11 +13,11 @@ import java.util.Locale;
 
 public class ExcelReader {
 
-    public static HashMap<String, Object> read(String[] args)
+    public static HashMap<String, Object> read(String filePath)
     {
         try
         {
-            String filePath = "/home/songtoan272/Documents/Cours/Semestre_5/Java_Bases/BitcoinMania/Project/Dataset/Fichier-Excel.xlsx";
+//            String filePath = "/home/songtoan272/Documents/Cours/Semestre_5/Java_Bases/BitcoinMania/Project/Dataset/Fichier-Excel.xlsx";
             File file = new File(filePath);   //creating a new file instance
             FileInputStream fis = new FileInputStream(file);   //obtaining bytes from the file
 
@@ -32,12 +32,14 @@ public class ExcelReader {
             data.put("Datetime", datetime);
             data.put("Price", price);
             SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss", Locale.ENGLISH);
-            for (int i=1; i<=10; i++){
+            for (int i=1; i<=sheet.getLastRowNum(); i++){
                 System.out.println(sheet.getRow(i).getCell(0).getDateCellValue());
                 System.out.println(sheet.getRow(i).getCell(1).getNumericCellValue());
                 datetime.add(sheet.getRow(i).getCell(0).getDateCellValue());
                 price.add(sheet.getRow(i).getCell(1).getNumericCellValue());
-                System.out.println("date: " + ((LinkedList<Date>) data.get("Datetime")).get(i-1) + "\n" +
+                System.out.println(
+                        "row: " + i + "\n" +
+                        "date: " + ((LinkedList<Date>) data.get("Datetime")).get(i-1) + "\n" +
                         "price: " + ((LinkedList<Double>) data.get("Price")).get(i-1));
             }
             return data;
