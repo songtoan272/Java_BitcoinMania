6160a84fdd6f02459de21dcf512fb5c60109ec2f
songtoan272 <toancs272@gmail.com>
2020-07-13 19:26:00 +0200

    Live graph auto update + threshold lines + tooltips + ExcelReader

--- Project/Lib/BitcoinApp/src/fxml/DashboardSceneController.java
+++ Project/Lib/BitcoinApp/src/fxml/DashboardSceneController.java
@@ -1,12 +1,19 @@
 package fxml;
 
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Cursor;
+import javafx.scene.Node;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.Chart;
 import javafx.scene.chart.LineChart;
+import javafx.scene.chart.NumberAxis;
 import javafx.scene.control.*;
 import javafx.scene.input.InputMethodEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.WindowEvent;
+import realtime.LineChartRT;
 
 public class DashboardSceneController {
 
@@ -14,7 +21,10 @@ public class DashboardSceneController {
     private MenuBar menuBar;
 
     @FXML
-    private MenuItem menuClose;
+    private MenuItem menuImportExcel;
+
+    @FXML
+    private Menu menuExport;
 
     @FXML
     private MenuItem menuExportSQL;
@@ -26,13 +36,13 @@ public class DashboardSceneController {
     private MenuItem menuExportPDF;
 
     @FXML
-    private MenuItem menuImportExcel;
+    private MenuItem menuClose;
 
     @FXML
     private ChoiceBox<?> scaleOption;
 
     @FXML
-    private ChoiceBox<String> currencyOption;
+    private ChoiceBox<?> currencyOption;
 
     @FXML
     private Slider topThreshold;
@@ -50,32 +60,62 @@ public class DashboardSceneController {
     private DatePicker endDate;
 
     @FXML
-    private LineChart<?, ?> chart;
+    private ToggleButton toggleCandle;
+
+    @FXML
+    private AnchorPane graphPane;
+
+    @FXML
+    private LineChartRT chartRT;
+
+    @FXML
+    private LineChart<String, Number> chartFromExcel;
+
 
     @FXML
     private void initialize(){
-        //defining the axes for chart
-//        chart.getXAxis().setLabel("Time");
-        chart.getXAxis().setAnimated(false); // axis animations are removed
-//        chart.getYAxis().setLabel("Value");
-        chart.getYAxis().setAnimated(false); // axis animations are removed
+        chartRT = new LineChartRT(new CategoryAxis(), new NumberAxis());
 
-        //creating the line chart with two axis created above
-        chart.setTitle("Realtime Bitcoin Price");
-        chart.setAnimated(false); // disable animations
-        chart.setLegendVisible(true);
+        chartFromExcel = new LineChart<String, Number>(new CategoryAxis(), new NumberAxis());
+        chartFromExcel.setTitle("Bitcoin Price");
+        chartFromExcel.getXAxis().setLabel("Datetime");
+        chartFromExcel.getYAxis().setLabel("Value");
+        chartFromExcel.setAnimated(false); // disable animations
+        chartFromExcel.setLegendVisible(true);
+        chartFromExcel.setVisible(false);
+
+        graphPane.getChildren().addAll(chartRT, chartFromExcel);
+        for (Node chart : graphPane.getChildren()){
+            ((Chart) chart).setPrefWidth(graphPane.getPrefWidth());
+            ((Chart) chart).setMaxWidth(graphPane.getMaxWidth());
+            ((Chart) chart).setMinWidth(graphPane.getMinWidth());
+
+            ((Chart) chart).setPrefHeight(graphPane.getPrefHeight());
+            ((Chart) chart).setMaxHeight(graphPane.getMaxHeight());
+            ((Chart) chart).setMinHeight(graphPane.getMinHeight());
+        }
+         chartRT.setVisible(true); chartRT.setCursor(Cursor.CROSSHAIR);
+         chartRT.addEventFilter(WindowEvent.WINDOW_CLOSE_REQUEST, windowEvent -> {
+            chartRT.stopUpdate();
+            windowEvent.consume();
+            Platform.exit();
+         });
+         chartRT.updateData();
+
+        
+
 
         //Set up choicebox currencyOption
-        ObservableList<String> curriencies = FXCollections.observableArrayList(
-                "Currency",
-                "USD",
-                "EUR",
-                "GBP");
-        currencyOption.setItems(curriencies);
-        currencyOption.getSelectionModel().select(0);
-        currencyOption.getSelectionModel().selectedItemProperty().addListener((v, oldV, newV) -> {
-            System.out.println("Currency changed to " + newV);
-        });
+//        ObservableList<String> curriencies = FXCollections.observableArrayList(
+//                "Currency",
+//                "USD",
+//                "EUR",
+//                "GBP");
+//        currencyOption.setItems(curriencies);
+//        currencyOption.getSelectionModel().select(0);
+//        currencyOption.getSelectionModel().selectedItemProperty().addListener((v, oldV, newV) -> {
+//            System.out.println("Currency changed to " + newV);
+//        });
 
         //Set up
 
@@ -87,7 +127,7 @@ public class DashboardSceneController {
     }
 
     @FXML
-    void exportEXCEL(ActionEvent event) {
+    void importEXCEL(ActionEvent event) {
 
     }
 
@@ -134,6 +174,17 @@ public class DashboardSceneController {
     @FXML
     void updateTopThreshold(InputMethodEvent event) {
 
+    }
+
+    @FXML
+    void switchGraph(ActionEvent event) {
+        if (toggleCandle.isSelected()) {
+            chartRT.setVisible(false);
+            chartFromExcel.setVisible(true);
+        }else{
+            chartRT.setVisible(true);
+            chartFromExcel.setVisible(false);
+        }
     }
 
 }
--- Project/Lib/BitcoinApp/src/realtime/Main.java
+++ Project/Lib/BitcoinApp/src/realtime/Main.java
@@ -1,21 +1,10 @@
 package realtime;
 
 import javafx.application.Application;
-import javafx.application.Platform;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.chart.LineChart;
-import javafx.scene.chart.XYChart;
 import javafx.stage.Stage;
 
-import java.io.FileInputStream;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.ThreadLocalRandom;
-import java.util.concurrent.TimeUnit;
 
 public class Main extends Application {
     final int WINDOW_SIZE = 100;
@@ -27,77 +16,22 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        primaryStage.setTitle("Login");
-
+        primaryStage.close();
         // Create the FXMLLoader
         FXMLLoader loader = new FXMLLoader();
         // Path to the FXML File
-        String fxmlDocPath = "./src/fxml/loginScreen.fxml";
-        FileInputStream fxmlStream = new FileInputStream(fxmlDocPath);
-
+        String fxmlDocPath = "../fxml/loginScreen.fxml";
         // Create the Pane and all Details
-        Stage loginScreen = (Stage) loader.load(fxmlStream);
+        Stage loginScreen = (Stage) FXMLLoader.load(getClass().getResource(fxmlDocPath));
 
-        // setup scene
-//        Scene scene = new Scene(loginScreen);
-//        primaryStage.setScene(scene);
-
-        // show the stage
+        // show the log in stage
         loginScreen.show();
-        primaryStage.show();
-//        LineChart<String, Double> chart = (LineChart<String, Double>) loader.getNamespace().get("chart");
-//        LineChart<String, Double> chart = (LineChart<String, Double>) ((AnchorPane)
-//                        ((SplitPane) root.getCenter()).getItems().get(1))
-//                        .getChildren();
-//        updateData(chart);
-
-
-//        System.out.println(( (RealtimeChart)
-//                        ((AnchorPane)
-//                        ((SplitPane) root.getCenter()).getItems().get(1))
-//                        .getChildren())
-//        );
-
 
     }
 
-    public void updateData(LineChart<String, Double> chart){
-//        defining a series to display data
-        XYChart.Series<String, Double> series = new XYChart.Series<>();
-        series.setName("Data Series");
-
-        ObservableList<XYChart.Series<String, Double>> data = FXCollections.observableArrayList();
-        data.add(series);
-        // add series to chart
-        chart.setData(data);
-
-        // this is used to display time in HH:mm:ss format
-        final SimpleDateFormat simpleDateFormat = new SimpleDateFormat("HH:mm:ss");
-
-        // setup a scheduled executor to periodically put data into the chart
-        scheduledExecutorService = Executors.newSingleThreadScheduledExecutor();
-
-        // put dummy data onto graph per second
-        scheduledExecutorService.scheduleAtFixedRate(() -> {
-            // get a random integer between 0-10
-            Double random = ThreadLocalRandom.current().nextDouble(10.0);
-
-            // Update the chart
-            Platform.runLater(() -> {
-                // get current time
-                Date now = new Date();
-                // put random number with current time
-                series.getData().add(new XYChart.Data<>(simpleDateFormat.format(now), random));
-
-                if (series.getData().size() > WINDOW_SIZE)
-                    series.getData().remove(0);
-            });
-        }, 0, 1, TimeUnit.SECONDS);
-    }
 
     @Override
     public void stop() throws Exception {
         super.stop();
-//        scheduledExecutorService.shutdownNow();
     }
 }
